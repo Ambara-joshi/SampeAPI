b2e0c4adef2a20b26493d86c3ec37ab4970c5878
Ambara Joshi <41628587+Ambara-joshi@users.noreply.github.com>
2026-03-28 17:29:04 +0530

    Pushing Docker file

--- TestProject-TodoApi/obj/Debug/net8.0/TestProject-TodoApi.AssemblyInfo.cs
+++ TestProject-TodoApi/obj/Debug/net8.0/TestProject-TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject-TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc86b56c58b2ae82683bd68c01343d596d2c4a4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4181017a1d1d966d96d5d26f50752d1ad06b06ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject-TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject-TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
