d774809b191d4d49e52db19a093a60235c34e656
Ambara Joshi <41628587+Ambara-joshi@users.noreply.github.com>
2026-03-29 16:31:42 +0530

    Done

--- TestProject-TodoApi/obj/Debug/net8.0/TestProject-TodoApi.AssemblyInfo.cs
+++ TestProject-TodoApi/obj/Debug/net8.0/TestProject-TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject-TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2e0c4adef2a20b26493d86c3ec37ab4970c5878")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69700728bbf0f4652969c5fc7ceed5d5be74ba8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject-TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject-TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
